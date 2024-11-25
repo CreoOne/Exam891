463dd899f08bbac57002a639a418a1cad23bac97
Tomasz Czarnecki <creo.one@gmail.com>
2024-11-25 16:52:25 +0100

    Added DefaultSerializer

---
+++ sources/Exam891.Core.Tests/Serialization/DerfaultSerializerTests.cs
@@ -0,0 +1,35 @@
+﻿using Exam891.Core.Serialization;
+
+namespace Exam891.Core.Tests.Serialization
+{
+    public sealed class DerfaultSerializerTests
+    {
+        private sealed record class Person (string Name);
+
+        [Fact]
+        public void Serialize_WithDefaultOptions_CorrectString()
+        {
+            // Arrange
+            var serializer = new DefaultSerializer();
+
+            // Act
+            var result = serializer.Serialize(new Person(Name: "Sotha Sil"));
+
+            // Assert
+            Assert.Equal("{\"name\":\"Sotha Sil\"}", result);
+        }
+
+        [Fact]
+        public void Deserialize_WithDefaultOptions_CorrectObject()
+        {
+            // Arrange
+            var serializer = new DefaultSerializer();
+
+            // Act
+            var result = serializer.Deserialize<Person>("{\"name\":\"Serjo Indoril Nerevar Mora\"}");
+
+            // Assert
+            Assert.Equal("Serjo Indoril Nerevar Mora", result?.Name);
+        }
+    }
+}
---
+++ sources/Exam891.Core/Serialization/DefaultSerializer.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+
+namespace Exam891.Core.Serialization
+{
+    internal sealed class DefaultSerializer
+    {
+        private readonly JsonSerializerOptions _jsonSerializerOptions;
+
+        public DefaultSerializer(JsonSerializerOptions jsonSerializerOptions)
+            => _jsonSerializerOptions = jsonSerializerOptions;
+
+        public DefaultSerializer()
+            : this(new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            })
+        { }
+
+        public string Serialize<T>(T value)
+            => JsonSerializer.Serialize(value, _jsonSerializerOptions);
+
+        public T? Deserialize<T>(string json)
+            => JsonSerializer.Deserialize<T>(json, _jsonSerializerOptions);
+    }
+}
