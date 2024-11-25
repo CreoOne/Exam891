5388b903dc9b9c7fcf553a853e79b0e8ce83b304
Tomasz Czarnecki <creo.one@gmail.com>
2024-11-25 21:27:45 +0100

    Added AvailabilityQuery

---
+++ sources/Exam891.Core.Tests/Queries/Availability/AvailabilityQueryTests.cs
@@ -0,0 +1,80 @@
+﻿using Exam891.Core.Bookings.Models;
+using Exam891.Core.Bookings.Repositories;
+using Exam891.Core.Hotels.Models;
+using Exam891.Core.Hotels.Repositories;
+using Exam891.Core.Queries.Availability;
+using Exam891.Core.Queries.Search;
+
+namespace Exam891.Core.Tests.Queries.Availability
+{
+    public sealed class AvailabilityQueryTests
+    {
+        [Fact]
+        public void GetAvailability_WhenNoBookings_ReturnsAllRooms()
+        {
+            // Arrange
+            const string hotelId = "hotelId";
+            const string roomType = "roomType";
+            var hotelsRepository = new NaiveHotelsRepository();
+            hotelsRepository.Add(new Hotel
+            {
+                Id = hotelId,
+                Name = "hotelName",
+                Rooms =
+                [
+                    new Room
+                    {
+                        RoomType = roomType,
+                        RoomId = "roomId"
+                    }
+                ]
+            });
+            var bookingsRepository = new NaiveBookingsRepository();
+            var searchQuery = new SearchQuery(hotelsRepository, bookingsRepository);
+            var availabilityQuery = new AvailabilityQuery(searchQuery);
+
+            // Act
+            var results = availabilityQuery.Availability(hotelId, DateOnly.MinValue, DateOnly.MaxValue, roomType);
+
+            // Assert
+            Assert.Equal(1, results);
+        }
+
+        [Fact]
+        public void GetAvailability_WhenBookings_ReturnsAvailableRooms()
+        {
+            // Arrange
+            const string hotelId = "hotelId";
+            const string roomType = "roomType";
+            var hotelsRepository = new NaiveHotelsRepository();
+            hotelsRepository.Add(new Hotel
+            {
+                Id = hotelId,
+                Name = "hotelName",
+                Rooms =
+                [
+                    new Room
+                    {
+                        RoomType = roomType,
+                        RoomId = "roomId"
+                    }
+                ]
+            });
+            var bookingsRepository = new NaiveBookingsRepository();
+            bookingsRepository.Add(new Booking
+            {
+                HotelId = hotelId,
+                Arrival = DateOnly.MinValue,
+                Departure = DateOnly.MaxValue,
+                RoomType = roomType,
+                RoomRate = "roomRate"
+            });
+            var searchQuery = new SearchQuery(hotelsRepository, bookingsRepository);
+            var availabilityQuery = new AvailabilityQuery(searchQuery);
+            // Act
+            var results = availabilityQuery.Availability(hotelId, DateOnly.MinValue, DateOnly.MaxValue, roomType);
+            // Assert
+            Assert.Equal(0, results);
+        }
+    }
+}
---
+++ sources/Exam891.Core/Queries/Availability/AvailabilityQuery.cs
@@ -0,0 +1,21 @@
+﻿using Exam891.Core.Queries.Search;
+
+namespace Exam891.Core.Queries.Availability
+{
+    internal sealed class AvailabilityQuery
+    {
+        private readonly ISearchQuery _searchQuery;
+
+        public AvailabilityQuery(ISearchQuery searchQuery)
+            => _searchQuery = searchQuery;
+
+        public int Availability(string hotelId, DateOnly date, string roomType)
+            => Availability(hotelId, date, date, roomType);
+
+        public int Availability(string hotelId, DateOnly from, DateOnly to, string roomType)
+            => _searchQuery.Search(hotelId, from, to, roomType)
+                .Select(result => result.AvailableRoomsCount)
+                .DefaultIfEmpty()
+                .Max();
+    }
+}
