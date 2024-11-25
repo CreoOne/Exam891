3637bf4491dc69f116b78d1c8b9344ca365b13fb
Tomasz Czarnecki <creo.one@gmail.com>
2024-11-25 23:46:09 +0100

    Added FileImport factories for BookingsRepository and HotelRepositories

---
+++ sources/Exam891.Core/Bookings/Factories/FileImportBookingsRepositoryFactory.cs
@@ -0,0 +1,19 @@
+﻿using Exam891.Core.Bookings.Models;
+using Exam891.Core.Bookings.Repositories;
+using Exam891.Core.Serialization;
+
+namespace Exam891.Core.Bookings.Factories
+{
+    public static class FileImportBookingsRepositoryFactory
+    {
+        public static IBookingsRepository Create(string bookingsFilePath)
+        {
+            var deserliazer = new DefaultSerializer();
+            var fileContent = File.ReadAllText(bookingsFilePath);
+            var bookings = deserliazer.Deserialize<Booking[]>(fileContent);
+            var bookingsRepository = new NaiveBookingsRepository();
+            bookingsRepository.Add(bookings ?? []);
+            return bookingsRepository;
+        }
+    }
+}
---
+++ sources/Exam891.Core/Hotels/Factories/FileImportHotelsRepositoryFactory.cs
@@ -0,0 +1,19 @@
+﻿using Exam891.Core.Hotels.Models;
+using Exam891.Core.Hotels.Repositories;
+using Exam891.Core.Serialization;
+
+namespace Exam891.Core.Hotels.Factories
+{
+    public static class FileImportHotelsRepositoryFactory
+    {
+        public static IHotelsRepository Create(string hotelsFilePath)
+        {
+            var deserliazer = new DefaultSerializer();
+            var fileContent = File.ReadAllText(hotelsFilePath);
+            var hotels = deserliazer.Deserialize<Hotel[]>(fileContent);
+            var hotelsRepository = new NaiveHotelsRepository();
+            hotelsRepository.Add(hotels ?? []);
+            return hotelsRepository;
+        }
+    }
+}
