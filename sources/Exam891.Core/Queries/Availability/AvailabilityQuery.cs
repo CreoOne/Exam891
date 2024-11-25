ea166d0ae416e62b37b37b16716c082c0127c546
Tomasz Czarnecki <creo.one@gmail.com>
2024-11-25 23:44:35 +0100

    Removed unused code from AvailabilityQuery

--- sources/Exam891.Core/Queries/Availability/AvailabilityQuery.cs
+++ sources/Exam891.Core/Queries/Availability/AvailabilityQuery.cs
@@ -2,15 +2,12 @@
 
 namespace Exam891.Core.Queries.Availability
 {
-    internal sealed class AvailabilityQuery : IAvailabilityQuery
+    public sealed class AvailabilityQuery : IAvailabilityQuery
     {
         private readonly ISearchQuery _searchQuery;
 
         public AvailabilityQuery(ISearchQuery searchQuery)
             => _searchQuery = searchQuery;
-
-        public int Availability(string hotelId, DateOnly date, string roomType)
-            => Availability(hotelId, date, date, roomType);
 
         public int Availability(string hotelId, DateOnly from, DateOnly to, string roomType)
             => _searchQuery.Search(hotelId, from, to, roomType)
--- sources/Exam891.Core/Queries/Availability/IAvailabilityQuery.cs
+++ sources/Exam891.Core/Queries/Availability/IAvailabilityQuery.cs
@@ -1,9 +1,8 @@
 ﻿
 namespace Exam891.Core.Queries.Availability
 {
-    internal interface IAvailabilityQuery
+    public interface IAvailabilityQuery
     {
         int Availability(string hotelId, DateOnly from, DateOnly to, string roomType);
-        int Availability(string hotelId, DateOnly date, string roomType);
     }
 }
