7f6cf5a9e79fa56e78eb03a79511372994ed7565
Tomasz Czarnecki <creo.one@gmail.com>
2024-11-26 00:43:21 +0100

    Added Program implementation

--- sources/Exam891.Cli/Program.cs
+++ sources/Exam891.Cli/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace Exam891.Cli
+﻿using Exam891.Cli.CommandLine;
+using Exam891.Core.Bookings.Factories;
+using Exam891.Core.Hotels.Factories;
+using Exam891.Core.Queries.Availability;
+using Exam891.Core.Queries.Search;
+
+namespace Exam891.Cli
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                var mainController = BuildMainController(args);
+
+                while (true)
+                {
+                    var commandLine = Console.ReadLine();
+
+                    if (string.IsNullOrEmpty(commandLine))
+                        return;
+
+                    var command = CommandParser.Parse(commandLine);
+                    var result = mainController.Execute(command);
+
+                    Console.WriteLine(result);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+        }
+
+        private static MainController BuildMainController(string[] args)
+        {
+            var parameters = ArgumentsParser.Parse(args);
+            var hotelsRepository = FileImportHotelsRepositoryFactory.Create(parameters.HotelsFilePath);
+            var bookingsRepository = FileImportBookingsRepositoryFactory.Create(parameters.BookingsFilePath);
+            var searchQuery = new SearchQuery(hotelsRepository, bookingsRepository);
+            var availabilityQuery = new AvailabilityQuery(searchQuery);
+            return new MainController(searchQuery, availabilityQuery);
         }
     }
 }
