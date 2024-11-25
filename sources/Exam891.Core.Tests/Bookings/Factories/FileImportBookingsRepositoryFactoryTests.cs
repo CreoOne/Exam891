86be36201e5cc4561e88949cb665b94ae9827ea5
Tomasz Czarnecki <creo.one@gmail.com>
2024-11-26 00:49:28 +0100

    Added unit tests for FileImport Bookings and Hotels factories

---
+++ sources/Exam891.Core.Tests/Bookings/Factories/FileImportBookingsRepositoryFactoryTests.cs
@@ -0,0 +1,20 @@
+﻿using Exam891.Core.Bookings.Factories;
+
+namespace Exam891.Core.Tests.Bookings.Factories
+{
+    public class FileImportBookingsRepositoryFactoryTests
+    {
+        [Fact]
+        public void Create_WithValidFilePath_ReturnsBookingsRepository()
+        {
+            // Arrange
+            var filePath = "bookings.json";
+
+            // Act
+            void Act() => FileImportBookingsRepositoryFactory.Create(filePath);
+
+            // Assert
+            Assert.Throws<FileNotFoundException>(Act);
+        }
+    }
+}
---
+++ sources/Exam891.Core.Tests/Hotels/Factories/FileImportHotelsRepositoryFactoryTests.cs
@@ -0,0 +1,20 @@
+﻿using Exam891.Core.Hotels.Factories;
+
+namespace Exam891.Core.Tests.Hotels.Factories
+{
+    public class FileImportHotelsRepositoryFactoryTests
+    {
+        [Fact]
+        public void Create_WithValidFilePath_ReturnsHotelsRepository()
+        {
+            // Arrange
+            var filePath = "hotels.json";
+
+            // Act
+            void Act() => FileImportHotelsRepositoryFactory.Create(filePath);
+
+            // Assert
+            Assert.Throws<FileNotFoundException>(Act);
+        }
+    }
+}
